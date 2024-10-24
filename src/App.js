a1953c42284b748ac991792bac2a4040fb242df0
MAbdullah369 <bscs22111@itu.edu.pk>
2024-10-24 09:55:04 +0500

    Few-Movies-added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar/Navbar';
-
+import Movies from './Movies/Movies';
 function App() {
   return (
    <>
    <Navbar/>
+   <Movies/>
    </>
   );
 }
---
+++ src/Movies/Movies.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Movies = () => {
+  return (
+    <div className='Movies'>
+        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/uL1dR0L65GZgy3h2gvZcxDu0bRG.jpg" alt="" /><span>Family Pack
+Oct 23, 2024</span>
+<img src="https://media.themoviedb.org/t/p/w220_and_h330_face/iRCgqpdVE4wyLQvGYU3ZP7pAtUc.jpg" alt="" /><span>Transformers One
+Sep 11, 2024</span>
+<img src="https://media.themoviedb.org/t/p/w220_and_h330_face/k42Owka8v91trK1qMYwCQCNwJKr.jpg" alt="" />
+<span>Venom: The Last Dance
+Oct 25, 2024</span>
+<img src="https://media.themoviedb.org/t/p/w220_and_h330_face/wTnV3PCVW5O92JMrFvvrRcV39RU.jpg" alt="" />
+</div>
+  )
+}
+
+export default Movies
