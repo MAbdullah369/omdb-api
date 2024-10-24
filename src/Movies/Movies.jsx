79dc987d055f7f644a93f6c16ffe4812ea5177f9
MAbdullah369 <bscs22111@itu.edu.pk>
2024-10-24 10:55:53 +0500

    search-bar+Movies-updates

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
-
+import React from 'react';
+import './Movies.css'
 const Movies = () => {
   return (
     <div className='Movies'>
-        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/uL1dR0L65GZgy3h2gvZcxDu0bRG.jpg" alt="" /><span>Family Pack
-Oct 23, 2024</span>
-<img src="https://media.themoviedb.org/t/p/w220_and_h330_face/iRCgqpdVE4wyLQvGYU3ZP7pAtUc.jpg" alt="" /><span>Transformers One
-Sep 11, 2024</span>
-<img src="https://media.themoviedb.org/t/p/w220_and_h330_face/k42Owka8v91trK1qMYwCQCNwJKr.jpg" alt="" />
-<span>Venom: The Last Dance
-Oct 25, 2024</span>
-<img src="https://media.themoviedb.org/t/p/w220_and_h330_face/wTnV3PCVW5O92JMrFvvrRcV39RU.jpg" alt="" />
-</div>
+      <div className='mov '>
+        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/uL1dR0L65GZgy3h2gvZcxDu0bRG.jpg" alt="" />
+        <span>Family Pack Oct 23, 2024</span>
+      </div>
+      <div className='mov '>
+        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/iRCgqpdVE4wyLQvGYU3ZP7pAtUc.jpg" alt="" />
+        <span>Transformers One Sep 11, 2024</span>
+      </div>
+      <div className='mov '>
+        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/k42Owka8v91trK1qMYwCQCNwJKr.jpg" alt="" />
+        <span>Venom: The Last Dance Oct 25, 2024</span>
+      </div>
+      <div className='mov'>
+        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/wTnV3PCVW5O92JMrFvvrRcV39RU.jpg" alt="" />
+        <span>Movie 4</span>
+      </div>
+      <div className='mov '>
+        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/b33nnKl1GSFbao4l3fZDDqsMx0F.jpg" alt="" />
+        <span>Movie 5</span>
+      </div>
+      <div className='mov '>
+        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg" alt="" />
+        <span>Movie 6</span>
+      </div>
+      <div className='mov '>
+        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/9akij7PqZ1g6zl42DQQTtL9CTSb.jpg" alt="" />
+        <span>Movie 7</span>
+      </div>
+      <div className='mov '>
+        <img src="https://media.themoviedb.org/t/p/w220_and_h330_face/lqoMzCcZYEFK729d6qzt349fB4o.jpg" alt="" />
+        <span>Movie 8</span>
+      </div>
+    
+    </div>
   )
 }
 
