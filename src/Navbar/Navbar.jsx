1f38621f0025957a9500aa924a3ec7b7bf55028f
MAbdullah369 <bscs22111@itu.edu.pk>
2024-10-24 10:07:01 +0500

    updated-navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import "./Navbar.css"
 const Navbar = () => {
   return (
-    <div  className='Navbar'>
-        <nav class="navbar">
-            <div class="logo">Movies API</div>
-            <ul class="nav-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">Movies Details</a></li>
-                <li><a href="#services">Favorites</a></li>
-            </ul>
-        </nav>
-    </div>
+    <div class='Navbar'>
+    <nav class="navbar">
+        <div class="logo">Movies API</div>
+        <ul class="nav-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">Movies Details</a></li>
+            <li><a href="#services">Favorites</a></li>
+        </ul>
+        <div class="search-container">
+            <input type="text" placeholder="Search..." class="search-input"/>
+            <button class="search-button">Search</button>
+        </div>
+    </nav>
+</div>
   )
 }
 
